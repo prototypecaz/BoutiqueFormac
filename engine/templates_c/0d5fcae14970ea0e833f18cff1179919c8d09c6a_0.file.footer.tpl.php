11221a405463e1cc8e9f0b8542c65b772ac70a75
Cazes <heycasimir82@hotmail.fr>
2023-09-09 16:34:04 +0200

    Initial commit

---
+++ engine/templates_c/0d5fcae14970ea0e833f18cff1179919c8d09c6a_0.file.footer.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2023-09-06 17:12:31
+  from '/var/www/html/boutique/engine/templates/front-office/footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_64f896df4c2de3_51913608',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '0d5fcae14970ea0e833f18cff1179919c8d09c6a' => 
+    array (
+      0 => '/var/www/html/boutique/engine/templates/front-office/footer.tpl',
+      1 => 1600438142,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_64f896df4c2de3_51913608 (Smarty_Internal_Template $_smarty_tpl) {
+?><footer class="text-center bg-dark p-3">
+    <p class='text-white h3'><?php echo $_smarty_tpl->tpl_vars['general']->value[0]['footer'];?>
+</p>
+
+
+    
+</footer><?php }
+}
